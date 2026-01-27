d4cf2e3d8f6a05af79d5925f90c41edf09d0b951
Richard Griffiths <rich.griffiths89@gmail.com>
2026-01-27 10:30:44 +0000

    debug: crypto issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const webpack = require("webpack");
+import { ProvidePlugin } from "webpack";
 
 const nextConfig = {
   images: {
@@ -17,7 +17,7 @@ const nextConfig = {
         crypto: require.resolve("crypto-browserify"),
       };
       config.plugins.push(
-        new webpack.ProvidePlugin({
+        new ProvidePlugin({
           Buffer: ["buffer", "Buffer"],
         })
       );
@@ -27,4 +27,4 @@ const nextConfig = {
   // output: "standalone",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
